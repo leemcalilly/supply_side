556c50ec5cb2bfbbdb929203ce8f9745daf0a054
Lee McAlilly <leemcalilly@gmail.com>
2012-01-31 16:26:20 -0500

    adding tests and title to the signup page

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "Users" do
+  
+  describe "Sign Up" do
+    before(:each) do
+      visit signup_path
+    end
+    
+    it "successfully routes to the signup page" do
+      current_path == "/signup"
+    end
+    
+    it "has the right title" do
+       page.should have_selector("title", :text => "Sign Up | Supply Side")
+    end
+    
+    it "has the right headline" do
+       page.should have_selector("h1", :text => "Sign Up")
+    end
+  end 
+  
+end
