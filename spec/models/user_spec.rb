6bca2e3f99a4e82a3a0d8da4a16696e52180603f
Lee McAlilly <leemcalilly@gmail.com>
2012-02-07 23:13:28 -0500

    adding tests for user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,12 +8,30 @@ describe User do
   it "has a valid factory" do
     Factory(:user).should be_valid
   end
-
-  it "should have an email address" do
-    @user.email.should_not == ""
+  
+  
+  describe "emails" do
+    it "should have an email address" do
+      @user.email.should_not == ""
+    end
+    
+    it "should reject duplicate email addresses" do
+      user_with_duplicate_email = Factory.build(:user, :email => "lee@example.com")
+      user_with_duplicate_email.should_not == @user.email
+    end
   end
   
-  it "should have a password" do
-    @user.password.should_not == ""
+  describe "passwords" do
+    it "should have a password" do
+      @user.password.should_not == ""
+    end
+  
+    it "should have a password confirmation" do
+      @user.password_confirmation.should_not == ""
+    end
+  
+    it "should have a password confirmation that equals the password" do
+      @user.password_confirmation == @user.password
+    end
   end
 end
