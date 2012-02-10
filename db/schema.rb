095b48ef3cf3c47d52ee41506c864eaeaf229937
Lee McAlilly <leemcalilly@gmail.com>
2012-02-09 22:54:24 -0500

    adding model for products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120131223504) do
+ActiveRecord::Schema.define(:version => 20120210035222) do
+
+  create_table "products", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
